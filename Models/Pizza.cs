c1670f04d899745cf52778262b77a4796ab24ada
Jonah Andersson <cjonah@hotmail.se>
2021-08-17 15:55:29 +0200

    code changes with CRUD examples

---
+++ Models/Pizza.cs
@@ -0,0 +1,9 @@
+namespace nnug_dotnet_challenge_contosopizza_.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsGlutenFree { get; set; }
+    }
+}
